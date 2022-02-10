36dab6843854622c9a34e3b689f9057e86a66533
Jantu Deb <jantud54@gmail.com>
2022-02-10 16:04:53 +0530

    feat: added toast and snackbar

--- components/navigation.js
+++ components/navigation.js
@@ -1,16 +1,37 @@
 const menuIcon = document.querySelector(".menu-icon");
 const mobileMenu = document.querySelector(".mobile");
+
+// Modal
 const showModal = document.querySelector("#show-modal");
 const modal = document.querySelector(".modal");
 const hideModal = document.querySelector("#btn-hide-modal");
 
+// Snackbar
+const btnBaseline = document.querySelector("#btn-baseline");
+const btnLeading = document.querySelector("#btn-leading");
+const btnStacked = document.querySelector("#btn-stacked");
+const btnClose = document.querySelectorAll(".snackbar-close");
+
+menuIcon?.addEventListener("click", () => toggle(mobileMenu));
+
+// modal
+showModal?.addEventListener("click", () => toggle(modal));
+hideModal?.addEventListener("click", () => toggle(modal));
+
+// snackbar
+btnBaseline?.addEventListener("click", () =>
+  toggle(document.querySelector("#baseline"))
+);
+
+btnLeading?.addEventListener("click", () =>toggle(document.querySelector("#leading")));
+
+btnStacked?.addEventListener("click", () => toggle(document.querySelector("#stacked")));
+
+btnClose?.forEach((el,i) => {
+  el.addEventListener("click", ()=>toggle(document.querySelectorAll(".snackbar")[i]));
+});
 
 function toggle(el) {
-  if (el.classList.contains("visibility"))
-    el.classList.remove("visibility");
+  if (el.classList.contains("visibility")) el.classList.remove("visibility");
   else el.classList.add("visibility");
 }
-
-menuIcon?.addEventListener("click",()=> toggle(mobileMenu));
-showModal?.addEventListener("click", ()=>toggle(modal));
-hideModal?.addEventListener("click",()=>toggle(modal))
